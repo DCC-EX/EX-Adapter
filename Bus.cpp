a9d7929f95b0d5d4a58475a43cac44c5f715cf2a
Alex Shepherd <kiwi64ajs@gmail.com>
2023-10-01 18:11:40 +1300

    added config.h from the ESP32CanLoadTest example and also pasted in various bits of code to get the node to connect to the LCC CAN Bus and do some initial things it definitely needs more testing and more work to make it functional but at least it doesn't crash immediately and I can chang ethe node name and description

--- Bus.cpp
+++ Bus.cpp
@@ -15,6 +15,37 @@ NOTES:
 #include <OpenMRNLite.h>
 #include <openlcb/CallbackEventHandler.hxx>
 #include <executor/Notifiable.hxx>
+#include <SPIFFS.h>
+#include <esp_spi_flash.h>
+
+#define USE_TWAI
+
+#include "config.h"
+
+/// ConfigDef comes from config.h and is specific to this particular device and
+/// target. It defines the layout of the configuration memory space and is also
+/// used to generate the cdi.xml file. Here we instantiate the configuration
+/// layout. The argument of offset zero is ignored and will be removed later.
+static constexpr openlcb::ConfigDef cfg(0);
+
+namespace openlcb
+{
+    // Name of CDI.xml to generate dynamically.
+    const char CDI_FILENAME[] = "/spiffs/cdi.xml";
+
+    // This will stop openlcb from exporting the CDI memory space upon start.
+    extern const char CDI_DATA[] = "";
+
+    // Path to where OpenMRN should persist general configuration data.
+    extern const char *const CONFIG_FILENAME = "/spiffs/openlcb_config";
+
+    // The size of the memory space to export over the above device.
+    extern const size_t CONFIG_FILE_SIZE = cfg.seg().size() + cfg.seg().offset();
+
+    // Default to store the dynamic SNIP data is stored in the same persistant
+    // data file as general configuration data.
+    extern const char *const SNIP_DYNAMIC_FILENAME = CONFIG_FILENAME;
+}
 
 using namespace openlcb;
 
@@ -25,8 +56,6 @@ Esp32HardwareTwai twai(CAN_RX_PIN, CAN_TX_PIN);
 const uint64_t NODE_ID = UINT64_C(0x05010101DCCE);  // TODO make configurable
 OpenMRN openmrn(NODE_ID);
 CallbackEventHandler * eventHandler;
-
-extern const char *const openlcb::SNIP_DYNAMIC_FILENAME = "Why do I need this"; //???
 
 /// Event handler called automatically for incomin event of interest.
 /// The user_bits are the Adapter-chosen id number to save a re-lookup.
@@ -69,6 +98,31 @@ void Bus::inboundEvents(uint64_t inbound[], int16_t count) {
      
 // Tell bus we are config complete and ready to play. 
 void Bus::ready() {
+      // Initialize the SPIFFS filesystem as our persistence layer
+    if (!SPIFFS.begin())
+    {
+        printf("SPIFFS failed to mount, attempting to format and remount\n");
+        if (!SPIFFS.begin(true))
+        {
+            printf("SPIFFS mount failed even with format, giving up!\n");
+            while (1)
+            {
+                // Unable to start SPIFFS successfully, give up and wait
+                // for WDT to kick in
+            }
+        }
+    }
+
+#if defined(USE_TWAI)
+    twai.hw_init();
+#endif // USE_TWAI
+
+    // Create the CDI.xml dynamically
+    openmrn.create_config_descriptor_xml(cfg, openlcb::CDI_FILENAME);
+
+    // Create the default internal configuration file
+    openmrn.stack()->create_config_file_if_needed(cfg.seg().internal_config(),
+        openlcb::CANONICAL_VERSION, openlcb::CONFIG_FILE_SIZE);
     // Add the hardware CAN device as a bridge
     openmrn.add_can_port_async("/dev/twai/twai0");
     openmrn.begin();
--- Ex-Adapter.ino
+++ Ex-Adapter.ino
@@ -1,6 +1,16 @@
 #include <Arduino.h>
 #include "Adapter.h"
 
+/* Sample DCC-EX Commands To Execute for Testing
+
+<LL 0 x0201570002B00302>
+<LL 1 x0201570002B00303>
+<LS x1122334455667788>
+<LD>
+<LR>
+
+*/
+
 void setup() {
     Adapter::setup();
 }
